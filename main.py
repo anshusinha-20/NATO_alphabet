7cdb4d6006b5aaaf9c9070330cd1a2a52dd69309
Anshu Sinha <anshujuly2@gmail.com>
2023-04-06 09:55:54 +0530

    modify

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-"""squaring numbers"""
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-squaredNumbers = [num ** 2 for num in numbers]
-print(squaredNumbers)
-
+# """squaring numbers"""
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# squaredNumbers = [num ** 2 for num in numbers]
+# print(squaredNumbers)
