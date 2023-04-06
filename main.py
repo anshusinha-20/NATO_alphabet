4bfedbd70dbc967720e3cf9d111b6b1abbfc6871
Anshu Sinha <anshujuly2@gmail.com>
2023-04-06 09:57:48 +0530

    Add a code to filter out even numbers using list comprehension

--- main.py
+++ main.py
@@ -2,3 +2,8 @@
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # squaredNumbers = [num ** 2 for num in numbers]
 # print(squaredNumbers)
+
+# """filtering even numbers"""
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# evenNumbers = [num for num in numbers if num % 2 == 0]
+# print(evenNumbers)
